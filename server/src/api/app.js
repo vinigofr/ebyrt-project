989d5005244efb17f1fb974e3d4f33de18b17782
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-10-31 18:42:17 -0300

    Configure app.js

--- server/src/api/app.js
+++ server/src/api/app.js
@@ -1,31 +1,15 @@
 const express = require('express');
 
 const app = express();
-
 app.use(express.json());
 
-// Aqui nesse espaço em branco serão implementados 
+// Imports here
+//
 
-const { MongoClient } = require("mongodb");
-const DB_URL= 'mongodb://127.0.0.1:27017';
-const DB_CONFIG = { useNewUrlParser: true, useUnifiedTopology: true };
+// Rotas
+app.use('fake route', 'route index');
 
-const client = new MongoClient(DB_URL, DB_CONFIG);
- 
-let db;
- 
-module.exports = {
-  connectToServer: function (callback) {
-    client.connect(function (err, db) {
-      if (db) {
-        db = db.db("myFirstDatabase");
-        console.log("Successfully connected to MongoDB."); 
-      }
-      return callback(err);
-});
-},
- 
-  getDb: function () {
-    return db;
-  },
-};
+// Middleware de erro
+//
+
+module.exports = app;
--- server/src/index.js
+++ server/src/index.js
@@ -9,15 +9,3 @@ app.use(express.json());
 
 app.use(require("./routes/record"));
 // get driver connection
-const dbo = require("./db/conn");
-
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
- 
-  });
-  console.log(`Server is running on port: ${port}`);
-});
