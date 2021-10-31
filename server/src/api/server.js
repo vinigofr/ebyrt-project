948e1300275b0694c54c90fe0ae9ed32e63fc39d
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-10-31 18:43:32 -0300

    Indentation on server.js

--- server/src/api/server.js
+++ server/src/api/server.js
@@ -1,4 +1,8 @@
 const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
+
+app.listen(
+  PORT,
+  () => console.log(`Server is running on port: ${PORT}`)
+);
