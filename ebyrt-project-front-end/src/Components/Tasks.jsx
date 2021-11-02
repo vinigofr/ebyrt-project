8d26a2f336c12e8bbc9dd65a3a1774c98b456c7b
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-11-02 19:15:38 -0300

    Add Task component and change PORT to 3001 in server

---
+++ ebyrt-project-front-end/src/Components/Tasks.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+async function Tasks() {
+  const [tasks, setTasks] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Tasks;
