a30111304071818604e0419e46857d9b4e80200a
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-11-02 15:43:38 -0300

    Add Header.jsx and Header.css

---
+++ ebyrt-project-front-end/src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Ebyrt Project Front End (Task Manager)</h1>
+    </header>
+  );
+}
+
+export default Header;
--- ebyrt-project-front-end/src/Pages/Main.jsx
+++ ebyrt-project-front-end/src/Pages/Main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import Header from '../Components/Header';
+
 function Main() {
   return (
     <div>
-      Hello World
+      <Header />
     </div>
   );
 }
