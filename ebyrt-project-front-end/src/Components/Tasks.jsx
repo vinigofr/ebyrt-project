ec482a9a9c10c1952ebe4c273d568dbd28269380
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-11-02 19:49:43 -0300

    CONGRAAATS! The first task rendering has made successfuly

--- ebyrt-project-front-end/src/Components/Tasks.jsx
+++ ebyrt-project-front-end/src/Components/Tasks.jsx
@@ -1,12 +1,31 @@
 import React, { useState, useEffect } from 'react';
+import Loading from './Loading';
 
-async function Tasks() {
+function Tasks() {
   const [tasks, setTasks] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  return (
+  useEffect(async () => {
+    async function fetchTasks() {
+      await fetch('http://localhost:3001/task/all')
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          setTasks(data);
+          setLoading(false);
+        });
+    }
+
+    fetchTasks();
+  }, [loading]);
+
+  return loading ? <Loading /> : (
     <div>
-      
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>{task.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
--- ebyrt-project-front-end/src/Pages/Main.jsx
+++ ebyrt-project-front-end/src/Pages/Main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Header from '../Components/Header';
+import Tasks from '../Components/Tasks';
 
 function Main() {
   return (
     <div>
       <Header />
+      <Tasks />
     </div>
   );
 }
