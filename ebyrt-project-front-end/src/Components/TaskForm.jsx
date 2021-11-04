de57491e141da9fd05cb6d830cf3a98a6020a3a0
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-11-04 18:44:08 -0300

    Change component name

--- ebyrt-project-front-end/src/Components/TaskForm.jsx
+++ ebyrt-project-front-end/src/Components/TaskForm.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function AddTaskForm() {
+function TaskForm() {
   return (
     <div>
       rendered task form
@@ -8,4 +8,4 @@ function AddTaskForm() {
   );
 }
 
-export default AddTaskForm;
+export default TaskForm;
