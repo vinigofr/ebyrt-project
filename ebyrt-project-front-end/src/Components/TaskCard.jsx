ed264ece1c6d32a1a490b0c97a9073c1a4f5ce75
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-11-04 00:26:37 -0300

    Scroll style done succefully for all task types containers

---
+++ ebyrt-project-front-end/src/Components/TaskCard.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function TaskCard({ task }) {
+  const {
+    title,
+    description,
+    status,
+    createdAt,
+  } = task;
+
+  return (
+    <div className="task-card">
+      <p>{`Título: ${title}`}</p>
+      <p>{`Descrição: ${description}`}</p>
+      <p>{`Status: ${status}`}</p>
+      <p>{`Criado em: ${createdAt}`}</p>
+    </div>
+  );
+}
+
+TaskCard.propTypes = {
+  task: PropTypes.shape({
+    title: PropTypes.string,
+    description: PropTypes.string,
+    status: PropTypes.string,
+    createdAt: PropTypes.string,
+  }).isRequired,
+};
+
+export default TaskCard;
--- ebyrt-project-front-end/src/Components/TaskType.jsx
+++ ebyrt-project-front-end/src/Components/TaskType.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Context from '../Context/Context';
+import TaskCard from './TaskCard';
+import '../Styles/General.css';
 
 function TaskType(props) {
   const { tasks } = React.useContext(Context);
-
   const { type } = props;
+
   return (
-    <div>
-      <h2>{type}</h2>
-      <div>
-        {tasks.map((task) => (<div>{ task.status === type ? task.title : null}</div>))}
+    <div className="task-type-container">
+      <div className="task-fixed-title">
+        <h2>{type}</h2>
+      </div>
+      <div className="task-type-cards">
+        {tasks.map((task) => (
+          <>
+            {
+            task.status === type
+              ? <TaskCard task={task} />
+              : null
+            }
+          </>
+        ))}
       </div>
     </div>
   );
