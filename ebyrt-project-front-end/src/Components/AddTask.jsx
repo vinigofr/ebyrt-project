6708be0d2665faab1799aefd4007cc3ea10a5d04
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-11-04 19:12:19 -0300

    Add title and description do state

--- ebyrt-project-front-end/src/Components/AddTask.jsx
+++ ebyrt-project-front-end/src/Components/AddTask.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
 
 function AddTask() {
+  const [task, setTask] = React.useState({
+    title: '',
+    description: '',
+  });
+
   return (
     <div className="add-task-field">
       <form>
-        <input type="text" placeholder="Título" />
-        <input type="text" placeholder="Descrição" />
+        <input
+          type="text"
+          name="title"
+          placeholder="Título"
+          onChange={(e) => setTask({ ...task, title: e.target.value })}
+        />
+        <input
+          type="text"
+          name="description"
+          placeholder="Descrição"
+          onChange={(e) => setTask({ ...task, description: e.target.value })}
+        />
       </form>
-      <button type="button">
+      <button
+        type="button"
+        onClick={() => console.log(task)}
+      >
         ➕ Nova Tarefa
       </button>
     </div>
