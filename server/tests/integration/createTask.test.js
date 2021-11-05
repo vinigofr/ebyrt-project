f9f1c832a3077766d53bb457b4da8a1a042f4da2
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-11-05 09:34:44 -0300

    Finish the first part of integration test

--- server/tests/integration/createTask.test.js
+++ server/tests/integration/createTask.test.js
@@ -1,10 +1,24 @@
 const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const server = require('../../src/api/app');
+
+chai.use(chaiHttp);
 
 const { expect } = chai;
 
 describe('POST /task/create', () => {
   describe('When created a task', () => {
     let response = {};
+
+    before(async () => {
+      response = await chai.request(server)
+        .post('/task/create')
+        .send({
+          title: 'Limpar a casa',
+          description: 'Começando pela cozinha!',
+        });
+    });
 
     it('Return status code 201', () => {
       expect(response).to.have.status(201);
@@ -17,6 +31,11 @@ describe('POST /task/create', () => {
     it('Return a object with the correct properties', () => {
       expect(response.body).to.have.all.keys('_id', 'title', 'description', 'status', 'createdAt');
     });
+
+    it('Return a object with the correct values', () => {
+      expect(response.body.title).to.be.equal('Limpar a casa');
+      expect(response.body.description).to.be.equal('Começando pela cozinha!');
+    })
   });
   
 });
