fff0e09e6ab97aa7ff5f87db9b1d294df126d0a8
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-11-05 10:12:46 -0300

    Finish more one case for tests

--- server/tests/integration/createTask.test.js
+++ server/tests/integration/createTask.test.js
@@ -50,7 +50,50 @@ describe('POST /task/create', () => {
     it('Return a object with the correct values', () => {
       expect(response.body.title).to.be.equal('Limpar a casa');
       expect(response.body.description).to.be.equal('Começando pela cozinha!');
-    })
+    });
   });
   
+  describe('When not insert params', () => {
+
+    let response = {};
+    const DBServer = new MongoMemoryServer();
+
+    before(async () => {
+      const OPTIONS = { useNewUrlParser: true, useUnifiedTopology: true };
+      const mockedURL = await DBServer.getUri();
+      const mockedConnection = await MongoClient.connect(mockedURL, OPTIONS);
+      
+      sinon.stub(MongoClient, 'connect').resolves(mockedConnection);
+
+      response = await chai.request(server)
+        .post('/task/create')
+        .send({
+          title: '',
+          description: '',
+        });
+    });
+
+    after(async () => {
+      MongoClient.connect.restore();
+      await DBServer.stop();
+    });
+
+    it('Return status code 400', () => {
+      expect(response).to.have.status(400);
+    });
+
+    it('Return a object', () => {
+      expect(response.body).to.be.a('object');
+    });
+
+    it('Return a object with the correct properties', () => {
+      expect(response.body).to.have.all.keys('error');
+      expect(response.body.error).to.have.all.keys('message');
+    });
+
+    it('Return a object with the correct value', () => {
+      expect(response.body.error.message).to.be.string;
+      expect(response.body.error.message).to.be.equal('All fields must be filled');
+    });
+  })
 });
