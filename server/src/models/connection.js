850e2c78c0c3f330a4e277bb6ed472dccadbc17c
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-11-05 00:31:52 -0300

    Add ENV server password

--- server/src/models/connection.js
+++ server/src/models/connection.js
@@ -6,6 +6,7 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+const REMOTE_URL = `mongodb+srv://vinigofr:${process.env.PASSWORD_DB}@viniciusprojects.whgqj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 const DB_URL = 'mongodb://127.0.0.1:27017';
 const DB_NAME = 'ebyrt';
 const DB_CONFIG = { useNewUrlParser: true, useUnifiedTopology: true };
@@ -13,7 +14,7 @@ const DB_CONFIG = { useNewUrlParser: true, useUnifiedTopology: true };
 let db = null;
 
 const getConnection = () => (db ? Promise.resolve(db)
-  : MongoClient.connect(DB_URL, DB_CONFIG).then((conn) => {
+  : MongoClient.connect(REMOTE_URL, DB_CONFIG).then((conn) => {
     db = conn.db(DB_NAME);
     return db;
   }));
